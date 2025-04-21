9b02a43a113dd7256d00b9ac140f26551d1fcfd5
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-21 17:39:40 +0530

    initial project setup commit

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser()); 
+
+app.listen(port, () => {
+    console.log("Server listening at port", port); 
+});
