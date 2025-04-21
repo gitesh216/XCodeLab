367cc4bccf095a95aca5596e6270ea5beacf99d8
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-21 18:23:56 +0530

    npx prisma migrate dev files

--- backend/src/generated/prisma/edge.js
+++ backend/src/generated/prisma/edge.js
@@ -151,7 +151,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -161,12 +161,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://myuser:mypassword@localhost:5432/postgres"
       }
     }
   },
--- backend/src/generated/prisma/index.js
+++ backend/src/generated/prisma/index.js
@@ -152,7 +152,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -162,12 +162,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://myuser:mypassword@localhost:5432/postgres"
       }
     }
   },
