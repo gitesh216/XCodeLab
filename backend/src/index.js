8960bab1d60ce9350e41924c689ede2568e681c5
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-21 18:30:28 +0530

    feat: setup of user auth boilerplate code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.routes";
 
 dotenv.config();
 
@@ -9,6 +11,12 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to XCodeLab")
+})
+
+app.use("/api/v1/auth", authRoutes)
 
 app.listen(port, () => {
     console.log("Server listening at port", port);
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const authRouter = express.Router();
+
+authRouter.post("/register")
+
+authRouter.post("/login")
+
+authRouter.post("/login")
+
+authRouter.post("/check")
+
+export default authRouter;
