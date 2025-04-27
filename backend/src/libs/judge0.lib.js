096b2bfcf14aff27507f37a844a2512b782f0e79
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-27 15:40:00 +0530

    feat: getlanguageId function

---
+++ backend/src/libs/judge0.lib.js
@@ -0,0 +1,10 @@
+const getJudge0LanguageId = (language) => {
+    const languageMap = {
+        "PYTHON": 71,
+        "JAVA": 62,
+        "JAVASCRIPT": 63,
+    }
+    return languageMap[language.toUpperCase()];
+}
+
+export { getJudge0LanguageId };
