1dde28fb15e51b5cab68947e6b5d372ed6dab287
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-27 15:07:17 +0530

    fix: import statement added

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.routes.js";
+import problemRoutes from "./routes/problem.routes.js"
 
 dotenv.config();
 
