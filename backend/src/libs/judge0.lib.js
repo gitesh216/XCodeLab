9639e841711170e042b94bd4d2c71780920b8b31
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-27 16:14:48 +0530

    feat: pollBatchResults for polling the batch results status code

--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -15,4 +15,30 @@ const submitBatch = async (submissions) => {
     return data;
 }
 
-export { getJudge0LanguageId, submitBatch };
+const sleep = (ms) => {
+    new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+const pollBatchResults = async (tokens) => {
+    while(true){
+        const { data } = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`, {
+            params: {
+                tokens: tokens.join(","),
+                base64_encoded: false
+            }
+        })
+
+        const results = data.submissions;
+
+        const isAllDone = results.every(
+            (r) => r.status.id !== 1 && r.status.id !== 2
+        )
+
+        if(isAllDone)
+            return results
+        
+        await sleep(1000);
+    }
+}
+
+export { getJudge0LanguageId, submitBatch, pollBatchResults };
