4b2035c6488735c578f48168e3493f9f04549f9e
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-27 14:57:29 +0530

    feat: check admin middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -35,7 +35,29 @@ const isUserValidAuthMiddleware = asyncHandler(async (req, res, next) => {
     }
 });
 
+const checkAdmin = async (req, res, next) => {
+    try {
+        const userId = req.user?.id;
+        const user = await db.user.findUnique({
+            where: {
+                id: userId
+            },
+            select: {
+                role: true
+            }
+        })
+        if(!user || user.role !== "ADMIN"){
+            throw new ApiError(403, "Access denied - Admin only")
+        }
+
+        next();
+    } 
+    catch (error) {
+        throw new ApiError(500, "Error in checkAdmin", error)
+    }
+}
 
 export {
     isUserValidAuthMiddleware,
+    checkAdmin
 }
