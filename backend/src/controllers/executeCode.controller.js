8e094588254695935214db8408de158e4f218dfd
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-29 18:33:53 +0530

    feat: executeCode endpoint for problem solution source code submission

--- backend/src/controllers/executeCode.controller.js
+++ backend/src/controllers/executeCode.controller.js
@@ -9,6 +9,41 @@ import {
 } from "../libs/judge0.lib.js";
 
 const executeCode = asyncHandler(async (req, res) => {
+    const { source_code, language_id, stdin, expected_outputs, problemId } = req.body;
+
+    const userId = req.user.id;
+    if(!userId){
+        throw new ApiError(400, "User id not found");
+    }
+
+    if(
+        !Array.isArray(stdin) ||
+        stdin.length === 0 ||
+        !Array.isArray(expected_outputs) ||
+        expected_outputs.length !== stdin.length
+    ){
+        return res.status(400).json(
+            new ApiResponse(400, null, "Invalid or missing test cases")
+        );
+    }
+
+    // prepare each test cases for Judge0 batch submission
+    const submissions = stdin.map((input) => ({
+        source_code,
+        language_id,
+        stdin: input,
+    }));
+
+    // send batch of submissions to Judge0
+    const submitResponse = await submitBatch(submissions);
+
+    const tokens = submitResponse.map((res) => res.token);
+
+    // Poll Judge0 for results of all submitted test cases
+    const results = await pollBatchResults(tokens);
+
+    console.log("Results-----------------------------");
+    console.log(results);
     
 });
 
