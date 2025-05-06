3c0c89889e8bf8b3a45afb16a3f53db9e561065e
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-05-06 21:58:34 +0530

    feat: create playlist controller logic

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -1,4 +1,7 @@
+import { db } from "../libs/db.js";
 import { asyncHandler } from "../utils/async-handler.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
 
 const getAllListDetails = asyncHandler(async(req, res) => {
 
@@ -9,7 +12,22 @@ const getPlayListDetails = asyncHandler(async(req, res) => {
 });
 
 const createPlaylist = asyncHandler(async(req, res) => {
+    const { name, description } = req.body;
+    const userId = req.user.id;
 
+    const playlist = await db.playlist.create({
+        data: {
+            name,
+            description,
+            userId
+        }
+    });
+    if(!playlist){
+        throw new ApiError(500, "Error in creating new playlist")
+    }
+    res.status(200).json(
+        new ApiResponse(200, playlist, "Playlist created successfully")
+    );
 });
 
 const addProblemToPlaylist = asyncHandler(async(req, res) => {
