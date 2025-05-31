78d39bd072707604b3c3de363478dcaa30ee8e34
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-05-31 10:26:38 +0530

    feat: add wrap home with layout route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,60 @@
-import React, { useEffect } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SignUpPage'
-import { userAuthStore } from './store/userAuthStore'
-import { Loader } from 'lucide-react'
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import { userAuthStore } from "./store/userAuthStore";
+import { Layout, Loader } from "lucide-react";
+import AdminRoute from "./components/AdminRoute";
 
 function App() {
-  const { authUser, checkAuth, isCheckingAuth } = userAuthStore()
-  
-  useEffect(() => {
-    checkAuth()
-  }, [checkAuth])
+  const { authUser, checkAuth, isCheckingAuth } = userAuthStore();
 
-  if(isCheckingAuth){
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth) {
     return (
-      <div className='flex items-center justify-center h-screen'>
-        <Loader className="size-10 animate-spin"/>
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
       </div>
-    )
+    );
   }
 
   return (
     <>
-    <div className='flex flex-col items-center justify-start'>
-      <Toaster /> 
-      <Routes>
+      <div className="flex flex-col items-center justify-start">
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={authUser ? <HomePage /> : <Navigate to="/login" />}
+            />
+          </Route>
 
-        <Route
-        path='/'
-        element={authUser ? <HomePage/> : <Navigate to="/login" />}
-        />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
 
-        <Route
-        path='/login'
-        element={!authUser ? <LoginPage/> : <Navigate to="/" />} 
-        />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route element={AdminRoute}/>
+          <Route 
+            path="/add-problem"
+            element={authUser ? <AddProblem /> : <Navigate to="/" />} 
+          />
 
-        <Route
-        path='/signup'
-        element={!authUser ? <SignUpPage/> : <Navigate to="/" />}
-        />
-      </Routes>
-    </div>
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
